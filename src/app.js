2e49e6328512081b2a3a0adfdf775ebfe3d1bccd
soujanya hazra <soujanya0105@gmail.com>
2022-03-23 13:34:41 +0530

    minor changes

--- src/app.js
+++ src/app.js
@@ -3,14 +3,22 @@ const app = express();
 const port = process.env.PORT || 5000;
 const path = require("path");
 const { urlencoded } = require("express");
+const views_path = path.join(__dirname, "../views");
+const static_path = path.join(__dirname, "../static");
 require("../src/db/connection");
 require('dotenv').config();
 app.use(express.json());
 app.use(urlencoded({ extended: false }));
+app.use("/static", express.static(static_path));
+
+
+app.set("view engine", "pug");
+app.set("views", views_path);
 
 //* Home page 
 app.get("/home",async(req,res)=>{
     // Home page will render here
+    res.status(200).render("index.pug");
 })
 
 //* Routes 
