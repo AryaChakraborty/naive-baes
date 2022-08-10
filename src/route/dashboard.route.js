cfaa73f1124986c99124256885f4ad9ad552124b
soujanya hazra <soujanya0105@gmail.com>
2022-08-10 22:56:00 +0530

    minor changes

--- src/route/dashboard.route.js
+++ src/route/dashboard.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Router = express.Router();
-const user = require("../model/user");
+const user = require("../model/user.model");
 Router.route("/dashboard")
     .get(async (req, res) => {
         res.status(200).render("dashboard.ejs");
