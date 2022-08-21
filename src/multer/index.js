256b71a209d5da0f7290410a7ead0b593d27a880
soujanya hazra <soujanya0105@gmail.com>
2022-08-21 12:14:46 +0530

    path changd

--- src/multer/index.js
+++ src/multer/index.js
@@ -1,16 +1,28 @@
 const multer = require("multer");
+const path = require("path");
+let i = 1;
 
 const storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null, path.join(__dirname, `../static/uploads/`));
+        callback(null, path.join(__dirname, `../../static/uploads`));
     },
     filename: function (req, file, callback) {
-        callback(null, file.originalname);
+        //* LicenseID to be taken from cookies
+        // callback(null, `${licenseID}-${i++}` + path.extname(file.originalname));
+        callback(null, `image-${i++}` + path.extname(file.originalname));
     }
 });
 
 const upload = multer({
-    storage: storage
+    storage: storage,
+    
+    // fileFilter: function (req, file, callback) {
+    //     let ext = path.extname(file.originalname);
+    //     if (ext !== '.pdf') {
+    //         return callback(null, false, new Error('Only pdf is allowed'));
+    //     }
+    //     callback(null, true);
+    // }
 });
 
-module.exports = { upload, storage }
+module.exports = { upload, storage };
