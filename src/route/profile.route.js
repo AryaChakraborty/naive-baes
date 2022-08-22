4a2ea8ef77d752468c3a2996864b61c989f0bd63
soujanya hazra <soujanya0105@gmail.com>
2022-08-22 22:58:38 +0530

    data fetched from database

--- src/route/profile.route.js
+++ src/route/profile.route.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const Router = express.Router();
-const user = require("../model/user.model");
+const userModel = require("../model/user.model");
+
 Router.route("/profile")
     .get(async (req, res) => {
-        res.status(200).render("profile.ejs");
+        try {
+
+            const licenseID = "Ajhdfui876hjkbj3249h87"; //TODO From cookies 
+
+            const user = await userModel.findOne({ licenseID });
+
+            const fullName = user?.fullName;
+            const email = user?.email;
+            const phoneNumber = user?.phoneNumber;
+            const position = user?.position;
+            const image = user?.image;
+
+            const param = {
+                "fullName": fullName,
+                "licenseID": licenseID,
+                "email":email,
+                "phoneNumber": phoneNumber,
+                "position": position,
+                "image": image,
+            };
+            res.status(200).render("profile.ejs", param);
+
+        }
+        catch (err) {
+            res.status(400).send(err);
+            console.log(err);
+        }
     })
 
 
