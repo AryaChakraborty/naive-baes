ee470015984733ba721f6736d1a5185778841ca9
soujanya hazra <soujanya0105@gmail.com>
2022-08-22 20:45:39 +0530

    file upload to aws added

--- src/route/submit.route.js
+++ src/route/submit.route.js
@@ -1,43 +1,33 @@
 const express = require("express");
 const Router = express.Router();
-const multer = require("multer");
-const judgement = require("../model/judgement.model")
-const { cloudinary, storage } = require("../cloudinary/index");
-const upload = multer({ storage });
+const judgement = require("../model/judgement.model");
+const { upload } = require("../multer/index");
+const { uploadFile } = require("../services/s3.service");
 
-
-// const cloudinaryImageUploadMethod = async file => {
-//     return new Promise(resolve => {
-//         cloudinary.uploader.upload(file, (err, res) => {
-//             if (err) return res.send("upload image error")
-//             resolve(res.secure_url)
-//         }
-//         )
-//     })
-// }
 
 Router.route("/submit")
     .get(async (req, res) => {
         res.render("submit.ejs");
-        // res.send("Submit Page")
     })
     .post(upload.array("documents", 100), async (req, res) => {
-        // const user = await judgement?.findOne({ licenseID });
 
-        let files = [];
         try {
 
+            let files = [];
             let filesold = req.files;
 
-            for (let item in filesold) {
+            for (let file of filesold) {
+
+                const result = await uploadFile(file);
+
                 let filesnew = {
-                    url: filesold[item].path,
-                    fileType: filesold[item].path.split(".")[filesold[item].path.split(".").length - 1],
+                    url: result.Location,
+                    fileType: file.mimetype,
                 }
                 files.push(filesnew);
             }
 
-            // console.log(files);
+            console.log(files);
 
 
             const newJudgement = new judgement({
@@ -47,8 +37,6 @@ Router.route("/submit")
                 documents: files
             });
 
-
-            // const dataSubmitted = await newJudgement.save(); //! Not working currently
 
             newJudgement.save((err, result) => {
                 if (err) {
@@ -70,12 +58,12 @@ Router.route("/submit")
 
         }
         catch (err) {
+            console.log(err);
             return res.status(500).json({
                 error: "true",
                 message: "Error before saving data",
             });
         }
-        // )
 
     })
 
