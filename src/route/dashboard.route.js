8e007bfa9d471605b07e2417053dc51d76c3a945
soujanya hazra <soujanya0105@gmail.com>
2022-08-23 13:06:51 +0530

    minor changes

--- src/route/dashboard.route.js
+++ src/route/dashboard.route.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const Router = express.Router();
 const user = require("../model/user.model");
+
 Router.route("/dashboard")
     .get(async (req, res) => {
         res.status(200).render("dashboard.ejs",
-        {
-            successMsg: req.flash("login-success")
-        });
+            {
+                successMsg: req.flash("login-success")
+            });
+    })
+
+    .post(async (req, res) => {
+        try {
+
+            const caseSearch = req.body.caseSearch;
+            console.log(caseSearch)
+            res.send(caseSearch);
+
+        } catch (err) {
+            console.log(err)
+        }
     })
 
 
