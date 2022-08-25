8e353e57702f637d57b9e7b553416a40fe3eb7d7
soujanya hazra <soujanya0105@gmail.com>
2022-08-25 20:32:19 +0530

    keyword result route added

---
+++ src/route/keyword.result.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const Router = express.Router();
+const user = require("../model/user.model");
+const axios = require("axios");
+
+Router.route("/keywordResult")
+    .get(async (req, res) => {
+        res.status(200).render("keywordResult.ejs"),{
+            "docs": docs
+        };
+    })
